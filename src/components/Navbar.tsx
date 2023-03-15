97df9922f1641c49238759fd40274463ae6f3f44
Neto <ukpongneto@wellahealth.com>
2023-03-15 11:10:31 +0100

    unused import removed from navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,22 @@
-import React, { useState } from "react";
-import close from "../assets/close.svg";
-import logo from "../assets/logo.svg";
-import menu from "../assets/menu.svg";
-import { navLinks } from "../constants/index";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import close from '../assets/close.svg';
+import logo from '../assets/logo.svg';
+import menu from '../assets/menu.svg';
+import { navLinks } from '../constants/index';
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <nav className="w-full flex py-6 justify-between items-center navbar">
-      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+      <img src={logo} alt='hoobank' className='w-[124px] h-[32px]' />
 
-      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+      <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
             className={`font-poppins font-normal cursor-pointer text-[16px] ${
-              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+              index === navLinks.length - 1 ? 'mr-0' : 'mr-10'
             } text-white`}
           >
             <a href={`#${nav.id}`}>{nav.title}</a>
@@ -27,25 +26,25 @@ const Navbar = () => {
 
       {/* mobile navbar view */}
 
-      <div className="sm:hidden flex flex-1 justify-end items-center">
+      <div className='sm:hidden flex flex-1 justify-end items-center'>
         <img
           src={toggle ? close : menu}
-          alt="nav menu"
-          className="w-[28px] h-[28px] object-contain"
+          alt='nav menu'
+          className='w-[28px] h-[28px] object-contain'
           onClick={() => setToggle((prev) => !prev)}
         />
 
         <div
           className={`${
-            toggle ? "flex" : "hidden"
+            toggle ? 'flex' : 'hidden'
           } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
         >
-          <ul className="list-none flex flex-col justify-end items-center flex-1">
+          <ul className='list-none flex flex-col justify-end items-center flex-1'>
             {navLinks.map((nav, index) => (
               <li
                 key={nav.id}
                 className={`font-poppins font-normal cursor-pointer text-[16px] ${
-                  index === navLinks.length - 1 ? "mr-0" : "mb-5"
+                  index === navLinks.length - 1 ? 'mr-0' : 'mb-5'
                 } text-white`}
               >
                 <a href={`#${nav.id}`}>{nav.title}</a>
